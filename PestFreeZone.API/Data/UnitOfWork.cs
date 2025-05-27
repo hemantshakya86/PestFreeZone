d4ad30fc6fe7d29f2b7e97938c7b55bcb5ac2d94
hemant shakya <hemant.shakya@quantyfy.com>
2025-05-27 20:11:20 +0530

    update ignore

--- PestFreeZone.API/Data/UnitOfWork.cs
+++ PestFreeZone.API/Data/UnitOfWork.cs
@@ -1,39 +1,61 @@
-﻿
-using Microsoft.Extensions.Options;
+﻿using Microsoft.EntityFrameworkCore;
 using PestFreeZone.API.Domain;
 using PestFreeZone.API.Domain.Models;
 using PestFreeZone.API.Services.Impl;
 using PestFreeZone.API.Services.Repository;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace PestFreeZone.API.Data
 {
     public class UnitOfWork : IUnitOfWork
     {
         private readonly ApplicationDbContext _context;
+        private readonly Dictionary<Type, object> _repositories = new();
 
-        private readonly Dictionary<Type, object> _repositories = new ();
         public UnitOfWork(ApplicationDbContext applicationDbContext)
         {
             _context = applicationDbContext;
         }
 
-      
-
         public async Task CommitChanges()
         {
-           
-            await _context.SaveChangesAsync();
+            using var transaction = await _context.Database.BeginTransactionAsync();
+            try
+            {
+                var entries = _context.ChangeTracker.Entries<BaseEntity>();
+                var utcNow = DateTime.UtcNow;
 
-            
+                foreach (var entry in entries)
+                {
+                    if (entry.State == EntityState.Added)
+                    {
+                        entry.Entity.CreatedDate = utcNow;
+                        entry.Entity.UpdatedDate = null;
+                    }
+                    else if (entry.State == EntityState.Modified)
+                    {
+                        entry.Entity.UpdatedDate = utcNow;
+                    }
+                }
+                await _context.SaveChangesAsync();
+                await transaction.CommitAsync();
+            }
+            catch (Exception)
+            {
+                await transaction.RollbackAsync();
+                throw; // Rethrow the exception for handling at a higher level
+            }
         }
 
         public IRepository<T> Repository<T>() where T : BaseEntity
         {
-           var type = typeof(T);
+            var type = typeof(T);
             if (!_repositories.ContainsKey(type))
             {
-                var repositoriesInstance= new Repository<T>(_context);
-                _repositories[type] = repositoriesInstance;
+                var repositoryInstance = new Repository<T>(_context);
+                _repositories[type] = repositoryInstance;
             }
             return (IRepository<T>)_repositories[type];
         }

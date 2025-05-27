4328c83de3f0215da0e67c652a0447c762817fc1
ravindra shakya <ravindra0360@gmail.com>
2025-05-27 15:35:21 +0530

    Refactoring the code

---
+++ PestFreeZone.API/Models/ContentPageModel.cs
@@ -0,0 +1,10 @@
+﻿namespace PestFreeZone.API.Models
+{
+    public class ContentPageModel
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public required string SubTitle { get; set; }
+        public required string Description { get; set; }
+    }
+}
--- PestFreeZone.API/Services/IContentService.cs
+++ PestFreeZone.API/Services/IContentService.cs
@@ -1,10 +1,12 @@
 ﻿using PestFreeZone.API.Domain;
+using PestFreeZone.API.Models;
 
 namespace PestFreeZone.API.Services
 {
     public interface IContentService
     {
-        Task<List<ContentPage>> GetAllContnet();
-        Task<bool> AddCotnet(ContentPage contentPage);
+        Task<List<ContentPageModel>> GetAllContent();
+        Task<bool> AddContent(ContentPageModel model);
+        Task<ContentPageModel> GetContentById(int id);
     }
 }
